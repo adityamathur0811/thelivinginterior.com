65539d96fe633ef725877ed037b3514f8d7d383c
adityamathur0811 <admathur08@gmail.com>
2020-06-20 23:28:22 +0530

    added some changes

--- app/src/main/java/thelivinginterior/com/RecyclerAdapter.java
+++ app/src/main/java/thelivinginterior/com/RecyclerAdapter.java
@@ -1,7 +1,6 @@
 package thelivinginterior.com;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
