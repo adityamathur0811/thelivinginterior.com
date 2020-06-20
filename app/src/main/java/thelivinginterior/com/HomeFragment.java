fc08a05e3da2e7989a5766e4dc758285b8e7a768
adityamathur0811 <admathur08@gmail.com>
2020-06-20 14:31:12 +0530

    aaaa

--- app/src/main/java/thelivinginterior/com/HomeFragment.java
+++ app/src/main/java/thelivinginterior/com/HomeFragment.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -42,65 +41,69 @@ public class HomeFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_home, container, false);
         viewPager=v.findViewById(R.id.viewpager);
         indicator=v.findViewById(R.id.indicator);
+
         viewPagerImages();
 
 
 
         return v;
     }
-    public void viewPagerImages()
-    {
-        picArray=new ArrayList<>();
-        demoPic=new ArrayList<>();
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReference().child("viewPagerPics")
-                .child("1.jpeg");
-
-        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-              
-                demoPic.add(uri.toString());
-                picArray.addAll(demoPic);
+    public void viewPagerImages() {
+        picArray = new ArrayList<>();
+        demoPic = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            String path = i + ".jpeg";
 
 
-                viewPager.setAdapter( new ViewPagerAdapter(getContext(),picArray));
-                indicator.setViewPager(viewPager);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("myApp","error");
-            }
-        });
+            FirebaseStorage storage = FirebaseStorage.getInstance();
 
+            StorageReference storageReference = storage.getReference().child("viewPagerPics")
+                    .child(path);
 
-        final Handler handler=new Handler();
-        final Runnable update=new Runnable() {
-            @Override
-            public void run()
-            {
-                if (pageCount==demoPic.size())
-                {
-                    pageCount=0;
+            storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+
+                    demoPic.add(uri.toString());
+                    picArray.addAll(demoPic);
+                    viewPager.setAdapter(new ViewPagerAdapter(getContext(), picArray));
+                    indicator.setViewPager(viewPager);
 
                 }
-                viewPager.setCurrentItem(pageCount++,true);
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("myApp", "error");
+                }
+            });
 
 
-            }
-        };
-        Timer timer=new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(update);
-            }
-        },3000,3000);
+        }
+
+            final Handler handler = new Handler();
+            final Runnable update = new Runnable() {
+                @Override
+                public void run() {
+                    if (pageCount == demoPic.size()) {
+                        pageCount = 0;
+
+                    }
+                    viewPager.setCurrentItem(pageCount++, true);
+
+
+                }
+            };
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(update);
+                }
+            }, 3000, 3000);
+
+
 
 
     }
-
-
 
 }
